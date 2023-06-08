e73476e021a7382f51665dbcf00e2dab980b98ee
Perry Ma <pengruima@gmail.com>
2023-06-08 12:13:24 -0700

    first

---
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String category_name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
