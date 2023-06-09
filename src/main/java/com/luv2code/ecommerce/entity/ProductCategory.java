7f8462235dead74b2f70a1c4c1563c88678e05e1
PRmars86 <pengruima@gmail.com>
2023-06-09 16:17:57 -0700

    category bar

--- src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -23,7 +23,7 @@ public class ProductCategory {
     private Long id;
 
     @Column(name = "category_name")
-    private String category_name;
+    private String categoryName;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
